79ce7fc6c0621725c86ca8952ce2b9754508c572
Hari <hrishikesh@Hrishikeshs-MacBook-Air.local>
2026-08-20 23:21:36 +0530

    Connect production frontend and backend

--- server/server.js
+++ server/server.js
@@ -24,7 +24,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://bite-that.vercel.app",
+    ],
     credentials: true,
   })
 );
